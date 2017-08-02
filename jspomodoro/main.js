67e816f8be60d55561b2c29b484c02a04c558f36
Omer Hefets <omerh127@gmail.com>
2017-08-02 23:23:07 +0300

    continued working on stop function

--- jspomodoro/main.js
+++ jspomodoro/main.js
@@ -1,3 +1,5 @@
+var inter;
+
 $(document).ready(function(){
   var min = $('#work').html();
   $('#min').html(min);
@@ -5,21 +7,36 @@ $(document).ready(function(){
 });
 
 $(document).on("click", ".buttoms", function(){
-  var min = $('#min').html() - 1;
-  var sec = 10;
-  var inter = setInterval(function(){
-    $('#sec').html(--sec);
-    $('#min').html(min);
+  var on = false;
+  var min = $('#min').html();
+  var sec = $('#sec').html();
+  $('.buttoms').html("stop");
+  $(this).addClass("stop_but");
+  $(this).removeClass("buttoms");
+  inter = setInterval(function(){
     if (sec == 0) {
       if (min == 0){
         clearInterval(inter);
-        var r_min = $('#rest').html();
-        $('#min').html(r_min);
+        on = true;
+        sec = 1;
+        min = $('#rest').html();
+        $('.stop_but').html("start_again");
       }
       else {
-      sec = 10;
+      sec = 5;
       min--;
       }
     }
+  $('#sec').html(--sec);
+  $('#min').html(min);
   }, 1000);
+  $(this).addClass("buttoms");
+  $(this).removeClass("stop_but");
+});
+
+$(document).on("click", ".stop_but", function(){
+  clearInterval(inter);
+  $('.stop_but').html("start");
+  $(this).addClass("buttoms");
+  $(this).removeClass("stop_but");
 });
